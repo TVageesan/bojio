cbcc222047ca60a75092645f46f1a7ba2b17ed1b
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-05-27 20:13:03 +0800

    successfuly deployed; reverting changes

--- BoJio/src/pages/IndexPage.vue
+++ BoJio/src/pages/IndexPage.vue
@@ -1,17 +1,23 @@
+<script setup>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+import { ref } from 'vue'
+
+const events = ref([]);
+</script>
+
+
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <vue-cal selected-date="2018-11-19"
+      :time-from="10 * 60"
+      :time-to="23 * 60"
+      :disable-views="['years', 'year']"
+      hide-view-selector
+      hide-weekends
+      :editable-events="{ title: false, drag: true, resize: true, delete: true, create: true }"
+      :events="events"
+      class="vuecal--full-height-delete">
+    </vue-cal>
   </q-page>
 </template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
-</script>

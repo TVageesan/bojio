aa3fc68e67c6ea09767293422b1d9d092cac4c32
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-05-28 15:45:17 +0800

    fixed env bug pt.2

--- BoJio/src/utils/supabaseClient.js
+++ BoJio/src/utils/supabaseClient.js
@@ -2,6 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-console.log('process test',process.env)
-
 export const supabase = createClient(supabaseUrl, supabaseKey);

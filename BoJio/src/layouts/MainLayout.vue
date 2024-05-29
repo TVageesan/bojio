213099b4aa13b633b748654cef943bcc3c5a7756
Nathan Tan <NathanTan@Nathans-MacBook-Pro-2.local>
2024-05-29 16:29:53 +0800

    added sidebar

--- BoJio/src/layouts/MainLayout.vue
+++ BoJio/src/layouts/MainLayout.vue
@@ -1,17 +1,38 @@
-<script setup>
-
-</script>
-
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
         <q-toolbar-title>
           Your Schedule
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
+
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :width="200"
+      :breakpoint="500"
+      bordered
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -19,4 +40,55 @@
   </q-layout>
 </template>
 
+<script>
+import { ref } from "vue";
 
+const menuList = [
+  {
+    icon: "inbox",
+    label: "Inbox",
+    separator: false,
+  },
+  {
+    icon: "send",
+    label: "Outbox",
+    separator: false,
+  },
+  {
+    icon: "delete",
+    label: "Trash",
+    separator: false,
+  },
+  {
+    icon: "error",
+    label: "Spam",
+    separator: false,
+  },
+  {
+    icon: "settings",
+    label: "Settings",
+    separator: false,
+  },
+  {
+    icon: "feedback",
+    label: "Send Feedback",
+    separator: false,
+  },
+  {
+    icon: "help",
+    iconColor: "primary",
+    label: "Help",
+    separator: false,
+  },
+];
+
+export default {
+  setup() {
+    const drawer = ref(false);
+    return {
+      drawer,
+      menuList,
+    };
+  },
+};
+</script>

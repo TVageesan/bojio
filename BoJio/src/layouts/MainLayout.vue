22c1c8590694522b6a141b755cf437df94af728f
Nathan Tan <NathanTan@Nathans-MacBook-Pro-2.local>
2024-05-30 22:17:54 +0800

    Changed sidebar type and added placeholder icon

--- BoJio/src/layouts/MainLayout.vue
+++ BoJio/src/layouts/MainLayout.vue
@@ -1,81 +1,80 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn flat @mouseenter="drawer = !drawer" round dense icon="menu" />
-        <q-toolbar-title>
-          Your Schedule
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="drawer"
-      show-if-above
-      :width="200"
-      :breakpoint="500"
-      bordered
-      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
-    >
-      <q-scroll-area class="fit">
-        <q-list>
-
-          <q-item header class = "text-h5 custom-header">
-            <b>BoJio</b>
-          </q-item>
-
-          <template v-for="(menuItem, index) in menuList" :key="index">
-            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
-              <q-item-section avatar>
-                <q-icon :name="menuItem.icon" />
-              </q-item-section>
-              <q-item-section>
-                {{ menuItem.label }}
-              </q-item-section>
-            </q-item>
-            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
-          </template>
-        </q-list>
-      </q-scroll-area>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
-</template>
-
-<script setup>
-import { ref } from "vue";
-
-const drawer = ref(false);
-
-const menuList = [
-  {
-    icon: "calendar_today",
-    label: "Calendar",
-    separator: false,
-  },
-  {
-    icon: "groups",
-    label: "Groups",
-    separator: false,
-  },
-  {
-    icon: "search",
-    label: "Browse",
-    separator: false,
-  },
-  {
-    icon: "event",
-    label: "Events",
-    separator: false,
-  },
-];
-</script>
-
-<style>
-.custom-header{
-  color:black;
-}
-</style>
+<template> 
+  <q-layout 
+    view="hHh Lpr lFf" 
+    container 
+    style="height: 800px" 
+    class="shadow-2 rounded-borders" 
+  > 
+    <q-header elevated :class="$q.dark.isActive ? 'bg-secondary' : 'bg-black'"> 
+      <q-toolbar> 
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" /> 
+        <q-toolbar-title 
+          ><q-icon name="mood" class="q-mr-sm" />Bojio</q-toolbar-title 
+        > 
+      </q-toolbar> 
+    </q-header> 
+ 
+    <q-drawer 
+      v-model="drawer" 
+      show-if-above 
+      :mini="miniState" 
+      @mouseover="miniState = false" 
+      @mouseout="miniState = true" 
+      :width="200" 
+      :breakpoint="500" 
+      bordered 
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'" 
+    > 
+      <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: 0 }"> 
+        <q-list padding> 
+          <template v-for="(menuItem, index) in menuList" :key="index"> 
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple> 
+              <q-item-section avatar> 
+                <q-icon :name="menuItem.icon" /> 
+              </q-item-section> 
+              <q-item-section> 
+                {{ menuItem.label }} 
+              </q-item-section> 
+            </q-item> 
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" /> 
+          </template> 
+        </q-list> 
+      </q-scroll-area> 
+    </q-drawer> 
+ 
+    <q-page-container> 
+      <router-view /> 
+    </q-page-container> 
+  </q-layout> 
+</template> 
+ 
+<script setup> 
+import { ref } from "vue"; 
+ 
+const drawer = ref(false); 
+const miniState = ref(true); 
+ 
+const menuList = [ 
+  { 
+    icon: "calendar_today", 
+    label: "Calendar", 
+    separator: false, 
+  }, 
+  { 
+    icon: "groups", 
+    label: "Groups", 
+    separator: false, 
+  }, 
+  { 
+    icon: "search", 
+    label: "Browse", 
+    separator: false, 
+  }, 
+  { 
+    icon: "event", 
+    label: "Events", 
+    separator: false, 
+  }, 
+]; 
+</script> 
+ 

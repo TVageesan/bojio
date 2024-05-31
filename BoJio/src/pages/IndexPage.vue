41b50ae56205429a1cbac1a520da609d0c0fa7c9
Nathan Tan <NathanTan@Nathans-MacBook-Pro-2.local>
2024-06-01 03:16:48 +0800

    added more fields to dialog

--- BoJio/src/pages/IndexPage.vue
+++ BoJio/src/pages/IndexPage.vue
@@ -52,28 +52,60 @@ const handleSubmit = () => {
   handleCreate(newEvt) // then send the created event to my calendar
 }
 //Also see below in template for rest of the dialog code
+
+const date = ref('2024-06-01 00:00')
+
 </script>
 
 <template>
   <!-- How I would've written it pt.2 -->
   <q-dialog v-model="openDialog">
-    <q-card>
+    <q-card style="min-width: 400px; min-height: 100px;">
       <q-card-section>
         <div class="text-h6">New Event</div>
       </q-card-section>
+      <div class="row justify-center">
+        <q-input v-model="eventName" label="Event Name" class="q-ma-md" style="width: 100%;"></q-input>
+        <q-input v-model="eventName" label="Start" class="q-ma-md" style="width: 100%;"></q-input>
+        <q-input v-model="eventName" label="End" class="q-ma-md" style="width: 100%;"></q-input>
+        <q-input v-model="eventName" label="Other Details" class="q-ma-md" style="width: 100%;"></q-input>
+      </div>
 
-      <q-card-section class="q-pt-none">
-        Event Name
-      </q-card-section>
+        <div class="q-pa-md" style="max-width: 300px">
+    <q-input filled v-model="date">
+      <template v-slot:prepend>
+        <q-icon name="event" class="cursor-pointer">
+          <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+            <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+              <div class="row items-center justify-end">
+                <q-btn v-close-popup label="Close" color="primary" flat />
+              </div>
+            </q-date>
+          </q-popup-proxy>
+        </q-icon>
+      </template>
 
-      <q-input label="example input" v-model="exampleInput"></q-input>
-
+      <template v-slot:append>
+        <q-icon name="access_time" class="cursor-pointer">
+          <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+            <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
+              <div class="row items-center justify-end">
+                <q-btn v-close-popup label="Close" color="primary" flat />
+              </div>
+            </q-time>
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+    </q-input>
+  </div>
+      
       <q-card-actions align="right">
         <q-btn color="primary" label="Add" @click="addEvent" />
           <q-btn color="negative" label="Cancel" @click="openDialog = false" />
       </q-card-actions>
     </q-card>
   </q-dialog>
+
   <div class="q-pa-md q-gutter-sm">
     <q-btn round color="black" icon="add" @click="openDialog = true" label="" />
   </div>
